580842512175eaa79155faa38267b799270114d4
SimonSteinberger <simon.steinberger@outlook.com>
2014-10-20 21:42:38 +0200

    Changed argument name for public methods
    Changed 'next' param to 'blur' in addTag and removeTag methods.

--- jquery.tag-editor.js
+++ jquery.tag-editor.js
@@ -15,7 +15,7 @@
     !function(e){e.fn.caret=function(e){var t=this[0],n="true"===t.contentEditable;if(0==arguments.length){if(window.getSelection){if(n){t.focus();var o=window.getSelection().getRangeAt(0),r=o.cloneRange();return r.selectNodeContents(t),r.setEnd(o.endContainer,o.endOffset),r.toString().length}return t.selectionStart}if(document.selection){if(t.focus(),n){var o=document.selection.createRange(),r=document.body.createTextRange();return r.moveToElementText(t),r.setEndPoint("EndToEnd",o),r.text.length}var e=0,c=t.createTextRange(),r=document.selection.createRange().duplicate(),a=r.getBookmark();for(c.moveToBookmark(a);0!==c.moveStart("character",-1);)e++;return e}return 0}if(-1==e&&(e=this[n?"text":"val"]().length),window.getSelection)n?(t.focus(),window.getSelection().collapse(t.firstChild,e)):t.setSelectionRange(e,e);else if(document.body.createTextRange)if(n){var c=document.body.createTextRange();c.moveToElementText(t),c.moveStart("character",e),c.collapse(!0),c.select()}else{var c=t.createTextRange();c.move("character",e),c.select()}return n||t.focus(),e}}(jQuery);
 
     // plugin with val as parameter for public methods
-    $.fn.tagEditor = function(options, val, next){
+    $.fn.tagEditor = function(options, val, blur){
 
         // build options dictionary with default values
         var blur_result, o = $.extend({}, $.fn.tagEditor.defaults, options), selector = this;
@@ -37,12 +37,12 @@
                     // insert new tag
                     $('<li><div class="tag-editor-spacer">&nbsp;'+o.delimiter[0]+'</div><div class="tag-editor-tag"></div><div class="tag-editor-delete"><i></i></div></li>').appendTo(ed).find('.tag-editor-tag')
                         .html('<input type="text" maxlength="'+o.maxLength+'">').addClass('active').find('input').val(val).blur();
-                    if (next != 'blur') ed.click();
+                    if (!blur) ed.click();
                     else $('.placeholder', ed).remove();
                 } else if (options == 'removeTag') {
                     // trigger delete on matching tag, then click editor to create a new tag
                     $('.tag-editor-tag', ed).filter(function(){return $(this).html()==val;}).closest('li').find('.tag-editor-delete').click();
-                    if (next != 'blur') ed.click();
+                    if (!blur) ed.click();
                 } else if (options == 'destroy') {
                     el.css('display', o.elDisplay).removeData('options').next('.tag-editor').remove();
                 }
